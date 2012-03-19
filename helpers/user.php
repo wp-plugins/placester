ce3687e51328b5e436b58551c9479e2c53180e83
Placester <Placester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-19 22:44:19 +0000

    git-svn-id: https://plugins.svn.wordpress.org/placester/trunk@521095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ helpers/user.php
@@ -0,0 +1,101 @@
+<?php 
+
+PL_Helper_User::init();
+class PL_Helper_User {
+	
+	public function init() {
+		add_action('sign-up-action', array(__CLASS__, 'set_admin_email' ) );
+		add_action('wp_ajax_set_placester_api_key', array(__CLASS__, 'set_placester_api_key' ) );
+		add_action('wp_ajax_existing_api_key_view', array(__CLASS__, 'existing_api_key_view' ) );
+		add_action('wp_ajax_new_api_key_view', array(__CLASS__, 'new_api_key_view' ) );
+		add_action('wp_ajax_create_account', array(__CLASS__, 'create_account' ) );
+		add_action('wp_ajax_user_empty_cache', array(__CLASS__, 'empty_cache' ) );
+		add_action('wp_ajax_user_save_global_filters', array(__CLASS__, 'set_global_filters' ) );
+		add_action('wp_ajax_ajax_log_errors', array(__CLASS__, 'ajax_log_errors' ) );
+	}
+
+	public static function set_admin_email (){
+		$_POST['email'] = get_option('admin_email');
+	}
+
+	public static function whoami($args = array()) {
+		return PL_User::whoami($args);
+	}
+
+	public static function existing_api_key_view () {
+		echo PL_Router::load_builder_partial('existing-placester.php');
+		die();
+	}
+
+	public static function new_api_key_view() {
+		self::set_admin_email();
+		echo PL_Router::load_builder_partial('sign-up.php');
+		die();	
+	}
+
+	public function set_placester_api_key() {
+		$result = PL_Option_Helper::set_api_key($_POST['api_key']);
+		echo json_encode($result);
+		die();
+	}
+
+	public function get_global_filters () {
+		$response = PL_Option_Helper::get_global_filters();
+		return array('filters' => $response);
+	}
+
+	public function set_global_filters () {
+		unset($_POST['action']);
+		$global_search_filters = PL_Validate::request($_POST, PL_Config::PL_API_LISTINGS('get', 'args'));
+		$response = PL_Option_Helper::set_global_filters(array('filters' => $global_search_filters));
+		if ($response) {
+			echo json_encode(array('result' => true, 'message' => 'You successfully updated the global search filters'));
+		} else {
+			echo json_encode(array('result' => false, 'message' => 'Change not saved or not change detected. Please try again.'));
+		}
+		die();
+	}
+
+	public function create_account() {
+		if ($_POST['email']) {
+			$response = PL_User::create(array('email'=>$_POST['email']) );
+			if ($response) {
+				echo json_encode($response);
+			} else {
+				echo json_encode(array(false, 'There was an error. Is that a valid email address?'));
+			}
+		} else {
+			echo json_encode(array(false, 'No Email Provided'));
+		}
+		die();
+	}
+
+	public function get_cached_items() {
+		return array('num_cached_items' => PL_Http::num_items_cached());
+	}
+
+	public function empty_cache() {
+		PL_Http::clear_cache();
+		echo json_encode(array('result' => true, 'message' => 'You\'ve successfully cleared your cache'));
+		die();
+	}
+
+	public function ajax_log_errors () {
+		if ( $_POST['report_errors'] == 'true') {
+			$report_errors = 1;
+		} else {
+			$report_errors = 0;
+		}
+		$api_response = PL_Option_Helper::set_log_errors($report_errors);
+		if ($api_response) {
+			if ($report_errors) {
+				echo json_encode(array('result' => true, 'message' => 'You successfully turned on error reporting'));
+			} else {
+				echo json_encode(array('result' => true, 'message' => 'You successfully turned off errror reporting'));
+			}
+		} else {
+			echo json_encode(array('result' => false, 'message' => 'There was an error. Please true again.'));
+		}
+		die();
+	}
+}
---
+++ views/admin/theme-gallery.php
@@ -0,0 +1,66 @@
+<?php //pls_dump(fetch_feed('https://placester.com/themes/feed/')) ?>
+<?php //pls_dump(wp_widget_rss_output('https://placester.com/themes/feed/')) ?>
+<?php  
+
+	$response = wp_remote_get("http://corporate.com/theme-api/", array('timeout' => 10));
+
+	foreach (json_decode($response['body']) as $key => $theme) {
+		// pls_dump($theme);
+	}
+
+/*	
+<form class="search-form filter-form" action="" method="get">
+	<!-- <p class="search-box">
+		<label class="screen-reader-text" for="theme-search-input">Search Installed Themes:</label>
+		<input type="text" id="theme-search-input" name="s" value="">
+		<input type="submit" name="" id="search-submit" class="button" value="Search Installed Themes">	<a id="filter-click" href="?filter=1">Feature Filter</a>
+	</p> -->
+	<br class="clear">
+	<table id="availablethemes" cellspacing="0" cellpadding="0">
+		<tbody id="the-list" class="list:themes">
+			<tr>
+				<td class="available-theme top left">
+					<a href="http://foundation.wpmulti.com/?preview=1&amp;template=chapman&amp;stylesheet=chapman&amp;preview_iframe=1&amp;TB_iframe=true&amp;width=640&amp;height=328" class="thickbox thickbox-preview screenshot">
+						<img src="http://foundation.wpmulti.com/wp-content/themes/chapman/screenshot.png" alt="">
+					</a>
+					<h3>Arthur Chapman Real Estate 1.0.0 by <a href="https://www.placester.com" title="Visit author homepage">The Placester Team</a></h3>
+					<p class="description">This is a custom WordPress theme created by Placester for Arthur Chapman Real Estate. Use with <a href="https://placester.com/">Placester</a>‘s <a href="wordpress.org/extend/plugins/placester/">Real Estate Builder plugin</a>.</p>
+					<span class="action-links"><a href="themes.php?action=activate&amp;template=chapman&amp;stylesheet=chapman&amp;_wpnonce=b9a7560b6c" class="activatelink" title="Activate “Arthur Chapman Real Estate”">Activate</a> | <a href="http://foundation.wpmulti.com/?preview=1&amp;template=chapman&amp;stylesheet=chapman&amp;preview_iframe=1&amp;TB_iframe=true&amp;width=640&amp;height=328" class="thickbox thickbox-preview" title="Preview “Arthur Chapman Real Estate”">Preview</a></span>
+					<p>All of this theme’s files are located in <code>/themes/chapman</code>.</p>
+					<p>Tags: blue, red, green, right-sidebar, fixed-width, custom-menu</p>
+				</td>
+			</tr>
+		</tbody>
+	</table>		
+</form>
+*/
+
+?>
+
+<style type="text/css">
+	.support_wrapper {
+	width: 600px;
+	margin: 50px auto;
+    font-family: "HelveticaNeue-Light","Helvetica Neue Light","Helvetica Neue",sans-serif;
+    font-weight: normal;
+    text-shadow: rgba(255, 255, 255, 1) 0 1px 0;
+    text-align: center;
+}
+.support_wrapper h1 {
+	font-size: 40px;
+	font-family: "HelveticaNeue-Light","Helvetica Neue Light","Helvetica Neue",sans-serif;
+    font-weight: normal;
+    text-shadow: rgba(255, 255, 255, 1) 0 1px 0;
+    margin-bottom: 40px;
+}
+.support_wrapper h3 {
+	font-size: 26px;
+	font-family: "HelveticaNeue-Light","Helvetica Neue Light","Helvetica Neue",sans-serif;
+    font-weight: normal;
+    text-shadow: rgba(255, 255, 255, 1) 0 1px 0
+}
+</style>
+<div class="support_wrapper">
+	<h1>Looking for Real Estate Themes?</h1>
+	<h3>Check them at the <a id="theme_gallery_placester" href="https://placester.com/themes/">theme gallery</a></h3>
+</div>
