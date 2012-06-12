d3ec3302c5f12ed7d24d4e6e730aa8efddcb1296
Placester <Placester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-12 22:03:22 +0000

    updated
    git-svn-id: https://plugins.svn.wordpress.org/placester/trunk@556958 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ helpers/compliance.php
@@ -0,0 +1,54 @@
+<?php 
+
+class PL_Compliance {
+
+	function mls_message ($args) {
+		extract(wp_parse_args($args, array('context' => false, 'agent_name' => false, 'office_name' => false)));
+		$whoami = PL_Helper_User::whoami();
+		// pls_dump($whoami);
+		if (!empty($whoami['provider']['disclaimer_on']) || !empty($whoami['provider']['office_on']) || !empty($whoami['provider']['agent_on'])) {
+			$provider = $whoami['provider'];
+			$response = array();
+
+			if ( $context == 'listings') {
+				$response['last_import'] = date_format(date_create($provider['last_import']), "jS F, Y g:i A.");
+				if (isset($provider['disclaimer_on']['listings']) && !empty($provider['disclaimer_on']['listings'])) {
+					$response['disclaimer'] = $provider['disclaimer'];	
+					$response['img'] = $provider['first_logo'];
+				}
+				if (isset($provider['agent_on']['listings']) && !empty($provider['agent_on']['listings']) && $agent_name) {
+					$response['agent_name'] = $agent_name;
+				}
+				if (isset($provider['office_on']['listings']) && !empty($provider['office_on']['listings']) && $office_name) {
+					$response['office_name'] = $office_name;
+				}
+			} elseif ( $context == 'search') {
+				$response['last_import'] = date_format(date_create($provider['last_import']), "jS F, Y g:i A.");
+				if (isset($provider['disclaimer_on']['search']) && !empty($provider['disclaimer_on']['search'])) {
+					$response['disclaimer'] = $provider['disclaimer'];	
+					$response['img'] = $provider['first_logo'];
+				}
+				if (isset($provider['agent_on']['search']) && !empty($provider['agent_on']['search']) && $agent_name) {
+					$response['agent_name'] = $agent_name;
+				}
+				if (isset($provider['office_on']['search']) && !empty($provider['office_on']['search']) && $office_name) {
+					$response['office_name'] = $office_name;
+				}
+			} elseif ( $context == 'inline_search') {
+				if (isset($provider['disclaimer_on']['inline_search']) && !empty($provider['disclaimer_on']['inline_search'])) {
+					$response['disclaimer'] = $provider['disclaimer'];	
+					$response['img'] = $provider['second_logo'];
+				}
+				if (isset($provider['agent_on']['inline_search']) && !empty($provider['agent_on']['inline_search']) && $agent_name) {
+					$response['agent_name'] = $agent_name;
+				}
+				if (isset($provider['office_on']['inline_search']) && !empty($provider['office_on']['inline_search']) && $office_name) {
+					
+					$response['office_name'] = $office_name;
+				}
+			}
+			return $response;
+		} 
+		return false;
+	}
+}
